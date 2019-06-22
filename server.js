a550fd68e9f35d9eeac23a6634869ef8a1e4cf7b
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-06-21 22:59:39 -0300

    Aula 02 -

--- server.js
+++ server.js
@@ -1,32 +1,5 @@
-const http = require('http');
-const servidor = http.createServer((req, resp) => {
-    let html = '';
-    if (req.url == '/') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-        `;
-    } else if (req.url == '/livros') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Listagem de Livros </h1>
-            </body> 
-        </html>
-        `;
-    }
+const app = require('./src/config/custom-express');
 
-    // outros else-if
-
-    resp.end(html);
+app.listen(3000, () => {
+    console.log(`Servidor rodando na porta 3000`);
 });
-servidor.listen(3000);
